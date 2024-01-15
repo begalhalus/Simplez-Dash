0557772975750780fe4c9df1767185d255e9c4f3
begalhalus <begalhalus@gmail.com>
2024-01-15 13:51:41 +0700

    feat: architecture FE

---
+++ webapps/src/components/global/index.ts
@@ -0,0 +1,9 @@
+export { Header } from "./Header";
+export { Navigation } from "./Navigation";
+export { Footer } from "./Footer";
+export { Layout, LoginLayout } from "./Layout";
+export { Pagination } from "./Pagination";
+
+export const API_URL = "https://hacklabs.rocks";
+export const APP_URL = `https://hacklabs.rocks`;
+export const APP_NAME = "Simplez Dash";
